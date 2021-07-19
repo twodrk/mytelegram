81512036831ab41fe859ebba5cf7de9dadc3c1ac
Anonymous <78583783+Anonymous-SK@users.noreply.github.com>
2021-07-19 14:20:07 +0530

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,22 @@
+class Translation(object):
+    START_TEXT = """Hi!
+Hai Iam a Simple My.telegram.org Bot.
+
+To Get API ID & API HASH Enter your Telegram Phone Number With Country Code.
+
+🤖Bot Update @NS_Bot_Update
+
+Click /Start To Restart The Progress"""
+    AFTER_RECVD_CODE_TEXT = """I see!
+Now please send the Telegram code that you received from Telegram!
+
+this code is only used for the purpose of getting the APP ID from my.telegram.org
+\nif you do not trust this bot dev, please host this bot yourself
+by opening https://github.com/Anonymous-SK/Telegram-ORG-NSBot
+
+Click /Start To Restart The Progress"""
+    BEFORE_SUCC_LOGIN = "recieved code. Scarpping web page ..."
+    ERRED_PAGE = "something wrongings. failed to get app id. \n\n@NS_Bot_Supporters\n\nHow Get Api Code For Website"
+    CANCELLED_MESG = "Bye! Please re /start the bot conversation"
+    IN_VALID_CODE_PVDED = "sorry, but the input does not seem to be a valid Telegram Web-Login code"
+    IN_VALID_PHNO_PVDED = "sorry, but the input does not seem to be a valid phone number
